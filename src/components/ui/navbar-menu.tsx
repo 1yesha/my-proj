6caa291bb09879bf434812c735218692a91cbce2
1yesha <ayeshasaleem786.com@gmail.com>
2024-10-02 20:39:41 -0700

    update

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
-import Navbar from "./Navbar";
+
 
 const transition = {
   type: "spring",
