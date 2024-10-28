412564bf37055b8e5685e8850911359faf1b2d88
1yesha <ayeshasaleem786.com@gmail.com>
2024-10-27 20:36:47 -0700

    update

--- src/components/cooltooltip.tsx
+++ src/components/cooltooltip.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
 import { AnimatedTooltip } from "./ui/animated-tooltip";
+
 const people = [
   {
     id: 1,
     name: "Ayesha Saleem",
     designation: "Content writer",
-    image: "/Ayesha.jpeg"
+    image: "/Ayesha.jpg"
     
   },
   {
@@ -18,11 +19,21 @@ const people = [
  
 ];
 
-export function cooltooltip() {
+ function cooltooltip() {
   return (
-    <div className="flex flex-row items-center justify-center mb-10 w-full">
-      <AnimatedTooltip items ={people} />
-    </div>
+   <div className="relative h-[40rem]  flex items-center justify-center  "
+   style={{ backgroundImage: 'url("/logo.jpeg")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' 
+     }}>
+    <div className="w-full max-w-7xl mx-auto flex 
+    flex-col items-center justify-center h-full">
+    <h2 className="text-2xl md:text-4xl lg:text-7xl
+    text-white font-bold text-center mb-8">Meet Our Instructors</h2>
+       <p className="text-base md:text-lg text-white text-center
+       mb-4" >Discover the talented professionals who will guide your coding journey</p>
+       <div className="flex flex-row items-center justify-center mb-10 w-full">
+       <AnimatedTooltip items ={people} /></div>
+       </div>
+       </div>
   );
 }
 
